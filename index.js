f2ffc02b4b46f0af3f3a8be20681fd5130678c10
Nery <136342009+Nery-25@users.noreply.github.com>
2024-02-07 20:42:46 +0000

    Servidor com setup express
    Implementação das rotas CRUD: Products

--- index.js
+++ index.js
@@ -1,9 +1,72 @@
-const express = require("express");
-const app = express();
 
-app.get("/", (req, res) => {
-  res.send("nothing to see here");
+const express = require('express');
+
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+
+
+
+let products = [];
+let idCounter = 1;
+
+// Listar todos os produtos (READ)
+app.get('/products', (req, res) => {
+  res.status(200).json(products);
 });
-app.listen(3000, () => {
-  console.log("server is running ");
+
+// Listar um produto por ID (READ)
+app.get('/products/:id', (req, res) => {
+  const productId = parseInt(req.params.id);
+  const product = products.find(p => p.id === productId);
+
+  if (!product) {
+    return res.status(404).json({ message: 'Produto não encontrado' });
+  }
+
+  res.status(200).json(product);
 });
+
+// Criar um novo produto (CREATE)
+app.post('/products', (req, res) => {
+  const newProduct = req.body;
+  newProduct.id = idCounter++;
+  products.push(newProduct);
+
+  res.status(201).json(newProduct);
+});
+
+// Atualizar um produto (UPDATE)
+app.put('/products/:id', (req, res) => {
+  const productId = parseInt(req.params.id); // Analisa se o argumento é uma string, se não for é converitdo em String
+  const productIndex = products.findIndex(p => p.id === productId);
+
+  if (productIndex === -1) {
+    return res.status(404).json({ message: 'Produto não encontrado' });
+  }
+
+  const updatedProduct = req.body;
+  products[productIndex] = updatedProduct;
+
+  res.status(200).json(updatedProduct);
+});
+
+// Excluir um produto (DELETE)
+app.delete('/products/:id', (req, res) => {
+  const productId = parseInt(req.params.id);
+  const productIndex = products.findIndex(p => p.id === productId);
+
+  if (productIndex === -1) {
+    return res.status(404).json({ message: 'Produto não encontrado' });
+  }
+
+  products.splice(productIndex, 1); // Altera  o array removendo  a posição do 
+
+  res.status(204).send();
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
