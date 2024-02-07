dfdb054258faea9daaa747b536cb9b57e47a91ee
GuilhermeMF <146479689+Nad1ah@users.noreply.github.com>
2024-02-07 21:51:22 +0000

    ..

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("nothing to see here");
-});
-app.listen(3000, () => {
-  console.log("server is running ");
-});
